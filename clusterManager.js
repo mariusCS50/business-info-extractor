893b752de21de59b1d26ec6084d7888f37c42bef
Marius <marius.gaibu@stud.acs.upb.ro>
2025-10-03 22:12:12 +0300

    Add puppeteer cluster manager for concurency

---
+++ clusterManager.js
@@ -0,0 +1,45 @@
+import { Cluster } from "puppeteer-cluster";
+import { Logger } from "./logger.js";
+
+let clusterPromise = null;
+let clusterInstance = null;
+
+const logger = new Logger("Cluster");
+
+export async function getCluster() {
+    if (!clusterPromise) {
+        clusterPromise = Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_PAGE,
+            maxConcurrency: 3,
+            puppeteerOptions: {
+                headless: false,
+				slowMo:50,
+                args: ["--no-sandbox", "--disable-setuid-sandbox"],
+            },
+            timeout: 60 * 1000,
+            retryLimit: 2,
+            retryDelay: 1000,
+        });
+
+        clusterInstance = await clusterPromise;
+
+        clusterInstance.on("taskerror", (err, data) => {
+            logger.error(`Failed crawling ${data}: ${err.message}`);
+        });
+
+		process.on("SIGINT", async () => {
+			logger.warn("SIGINT received, closing cluster...");
+			if (clusterInstance) await clusterInstance.close();
+			process.exit(0);
+		});
+
+		process.on("SIGTERM", async () => {
+			logger.warn("SIGTERM received, closing cluster...");
+			if (clusterInstance) await clusterInstance.close();
+			process.exit(0);
+		});
+
+    }
+
+    return clusterPromise;
+}
